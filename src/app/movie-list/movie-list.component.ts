59428278d9466418f62586d01cdafe6bbca603e6
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-08 10:38:51 -0700

    show a table of movies, using data-down action-up.

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../models/movie.model';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
-export class MovieListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class MovieListComponent {
+  @Input() childMovieList: Movie[];
 
 }
